cb9e3286475dec987aec9bdd73e0460246b2463c
Cameron Bradley <cameron@thebugexchange.com>
2017-11-06 10:46:43 +1100

    added fix for special excluded tags - default @broken

--- support/tag_manager.js
+++ support/tag_manager.js
@@ -3,16 +3,16 @@ var navigation = require('./navigation');
 var tagsForEnvironment = function (suite_tag) {
     switch(navigation.getEnvironment()) {
         case 'localhost':
-            var tags = [suite_tag, '~@wip', '~@removed', '~@unreleased', process.env.SPECIAL_EXCLUDED_TAGS];
+            var tags = [suite_tag, '~@wip', '~@removed', '~@unreleased', process.env.SPECIAL_EXCLUDED_TAGS || '~@broken'];
             break;
         case 'uat':
-            var tags = ['@uat', '~@wip', '~@removed', '~@unreleased', process.env.SPECIAL_EXCLUDED_TAGS];
+            var tags = ['@uat', '~@wip', '~@removed', '~@unreleased', process.env.SPECIAL_EXCLUDED_TAGS || '~@broken'];
             break;
         case 'preprod':
-            var tags = ['@preprod', '~@wip', '~@removed', '~@unreleased', process.env.SPECIAL_EXCLUDED_TAGS];
+            var tags = ['@preprod', '~@wip', '~@removed', '~@unreleased', process.env.SPECIAL_EXCLUDED_TAGS || '~@broken'];
             break;
         case 'production':
-            var tags = ["@production", '~@wip', '~@removed', '~@unreleased', process.env.SPECIAL_EXCLUDED_TAGS];
+            var tags = ["@production", '~@wip', '~@removed', '~@unreleased', process.env.SPECIAL_EXCLUDED_TAGS || '~@broken'];
             break;
     }
     return tags

bdc9e7d39a623a5693cbf6dd8780c328bc360625
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2018-02-07 10:20:08 +1100

    boost timeout

--- step_definitions/env.js
+++ step_definitions/env.js
@@ -2,4 +2,4 @@ const path = require('path');
 const ROOT_PATH = path.resolve('./');
 const {setDefaultTimeout} = require(ROOT_PATH + '/node_modules/cucumber');
 
-setDefaultTimeout(60 * 1000);
+setDefaultTimeout(60 * 10000);

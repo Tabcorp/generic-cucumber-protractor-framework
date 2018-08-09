58dee4c8f9462ecf6492454fa5fdf6e07bade903
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2018-08-09 13:28:56 +1000

    add synk test

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cat << EOM
+steps:
+  - name: ":cucumber: snyk test"
+    command: scripts/buildkite-deploy-pr.sh
+    agents:
+      queue: keno-web-auto
+EOM
---
+++ scripts/buildkite-deploy-pr.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+. /var/lib/buildkite-agent/.bash_profile
+
+echo "run synk test"
+synk test

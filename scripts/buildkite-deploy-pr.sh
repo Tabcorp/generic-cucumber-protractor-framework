8990bf7ccb437babe0430d6c6aca6f80dfab7506
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2018-08-09 14:21:59 +1000

    buikdkite-pipeline

--- scripts/buildkite-deploy-pr.sh
+++ scripts/buildkite-deploy-pr.sh
@@ -2,5 +2,16 @@
 set -e
 . /var/lib/buildkite-agent/.bash_profile
 
+echo "current node"
+echo node -v
+
+echo "Use node LTS/Boron 6.12.3 version"
+nvm install 6.12.3
+nvm use 6.12.3
+
+echo "run npm install"
+npm install
+
 echo "run synk test"
-synk test
+npm install -g snyk
+snyk test

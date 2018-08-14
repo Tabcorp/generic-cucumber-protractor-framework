58fd0f1bcfb61ee2b2db5906b7c8d13289406111
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2018-08-14 11:31:44 +1000

    update-before-hook-bump-1-7-7

--- step_definitions/hook.js
+++ step_definitions/hook.js
@@ -8,19 +8,28 @@ const json_store = require('../support/json_store')
 
 Before(function (scenario, next) {
     var tags = scenario.pickle.tags;
-    var browser_type = tags[0].name
+    var browser_type = tags[0].name;
     console.log("Resolution ", browser_type);
 
-    //set the browser dimensions for the test
+    // Be careful - we assume the arguments are in the unusual form [ height, width ]
+    function genericResize(args) {
+        window.resizeTo(args[1], args[0]);
+    }
+
     browser.params.browser_type = browser_type;
     if (browser_type === "@mobile") {
-        browser.driver.manage().window().setSize(parseInt(process.env.MOBILE_UI_HEIGHT) || 375, parseInt(process.env.MOBILE_UI_WIDTH) || 667);
+        var mobileSize = [ parseInt(process.env.MOBILE_UI_HEIGHT) || 375, parseInt(process.env.MOBILE_UI_WIDTH) || 667 ];
+        browser.executeScript(genericResize, mobileSize).then(next);
     } else if (browser_type === "@tablet") {
-        browser.driver.manage().window().setSize(parseInt(process.env.TABLET_UI_HEIGHT) || 768, parseInt(process.env.TABLET_UI_WIDTH) || 1024);
+        var tabletSize = [ parseInt(process.env.TABLET_UI_HEIGHT) || 768, parseInt(process.env.TABLET_UI_WIDTH) || 1024 ];
+        browser.executeScript(genericResize, tabletSize).then(next);
     } else if (browser_type === "@desktop") {
-        browser.driver.manage().window().setSize(parseInt(process.env.DESKTOP_UI_HEIGHT) || 1200, parseInt(process.env.DESKTOP_UI_WIDTH) || 800);
+        var dekstopSize = [ parseInt(process.env.DESKTOP_UI_HEIGHT) || 1200, parseInt(process.env.DESKTOP_UI_WIDTH) || 800 ];
+        browser.executeScript(genericResize, desktopSize).then(next);
     }
-    next();
+    else {
+        next();
+    }
 });
 
 // After hook for each scenario

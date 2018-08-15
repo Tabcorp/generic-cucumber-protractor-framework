45d006bd65c71a5c59ff326680b34855d8b0e9c8
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2018-08-15 14:27:30 +1000

    bump-framework-1.8.0

--- support/capabilities_manger.js
+++ support/capabilities_manger.js
@@ -27,4 +27,25 @@ const getBrowserCapabilities = function (browser, browser_arguments, browser_pre
 
 };
 
+const getBrowserResolution = function (resolution) {
+
+    switch(resolution) {
+        case '@mobile':
+            browser_resolution = (process.env.MOBILE_UI_HEIGHT || 375) + ',' + (process.env.MOBILE_UI_WIDTH || 667);
+            break;
+        case '@tablet':
+            var browser_resolution = (process.env.TABLET_UI_HEIGHT || 768) + ',' + (process.env.TABLET_UI_WIDTH || 1024);
+            break;
+        case '@desktop':
+            var browser_resolution = (process.env.DESKTOP_UI_HEIGHT || 1200) + ',' + (process.env.DESKTOP_UI_WIDTH || 800);
+            break;
+    };
+
+    console.log("Device Resolution Type: "+ resolution, "Browser resolution: "+ browser_resolution);
+
+    return "--window-size="+browser_resolution;
+};
+
+module.exports.getBrowserResolution = getBrowserResolution;
 module.exports.getBrowserCapabilities = getBrowserCapabilities;
+

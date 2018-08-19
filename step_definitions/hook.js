6807600e963fa2d743c606432c6d54e823139b1a
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2018-08-20 09:38:25 +1000

    minor fix for push tags

--- step_definitions/hook.js
+++ step_definitions/hook.js
@@ -3,12 +3,12 @@ const path = require('path');
 const ROOT_PATH = path.resolve('./');
 var {After, Before} = require(ROOT_PATH + '/node_modules/cucumber');
 
-const stored_data = require('../support/stored_data')
-const json_store = require('../support/json_store')
+const stored_data = require('../support/stored_data');
+const json_store = require('../support/json_store');
 
 // After hook for each scenario
 After(function (scenario, next) {
-    stored_data.deleteData()
+    stored_data.deleteData();
     json_store.deleteJson();
     next();
 });
